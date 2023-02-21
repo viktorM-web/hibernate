5da6061d3efff4d1dc3a72018ab26f02433ae301
Viktor <vitya.malahov.95@gmail.com>
2023-02-21 09:58:45 +0200

    add files from lesson-5

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import org.example.entity.UserEntity;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.concurrent.BlockingQueue;
 
 public class HibernateRunner {
@@ -22,11 +25,25 @@ public class HibernateRunner {
 //        Session
 
         Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(UserEntity.class);
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+
+            session.beginTransaction();
+
+            UserEntity user = UserEntity.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000,1,19))
+                    .build();
+
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
