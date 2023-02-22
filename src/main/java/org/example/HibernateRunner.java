75967b5ba95b4b7c4fabe98372a9ae73965cdd64
Viktor <vitya.malahov.95@gmail.com>
2023-02-22 10:35:39 +0200

    add files from lesson-17 (logging)

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -1,64 +1,44 @@
 package org.example;
 
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-import org.example.converter.BirthdayConverter;
-import org.example.entity.Birthday;
-import org.example.entity.Role;
 import org.example.entity.UserEntity;
 import org.example.util.HibernateUtil;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.concurrent.BlockingQueue;
 
 public class HibernateRunner {
 
-    public static void main(String[] args) throws SQLException {
+    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
 
+    public static void main(String[] args) throws SQLException {
         UserEntity user = UserEntity.builder()
                 .username("ivan@gmail.com")
                 .lastname("Ivanov")
                 .firstname("Ivan")
                 .build();
+        log.info("User entity is in transient state, object: {}", user);
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
-            try (Session session1 = sessionFactory.openSession()) {
-                session1.beginTransaction();
+            Session session1 = sessionFactory.openSession();
+            try (session1) {
+                Transaction transaction = session1.beginTransaction();
+                log.trace("Transaction is created, {}", transaction);
 
                 session1.saveOrUpdate(user);
+                log.trace("User is in persistent state: {}, session {}", user, session1);
 
                 session1.getTransaction().commit();
             }
-            try (Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                user.setFirstname("Sveta");
-
-//                session2.refresh(user);
-//
-////                UserEntity freshUser = session2.get(UserEntity.class, user.getUsername());
-////                user.setFirstname(freshUser.getFirstname());
-////                user.setLastname(freshUser.getLastname());
-//
-////                **how work refresh**
-
-                Object mergeUser = session2.merge(user);
-
-//                UserEntity freshUser = session2.get(UserEntity.class, user.getUsername());
-//                freshUser.setFirstname(user.getFirstname());
-//                freshUser.setLastname(user.getLastname());
-
-//                **how work merge**
-
-
-                session2.getTransaction().commit();
-            }
+            log.warn("User is in detached state: {}, session is closed {}", user, session1);
+        } catch (Exception exception) {
+            log.error("Exception occurred", exception);
+            throw exception;
         }
     }
 }
